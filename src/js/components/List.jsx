d47edd6e77c57553038b24e3fe7bf644c10d9ed3
Carlos Schile <esteban.ossandon20@gmail.com>
2025-09-17 11:53:07 -0300

    ya se carga el contenido

--- src/js/components/List.jsx
+++ src/js/components/List.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 //create your first component
 const List = () => {
+	const [ inputValue, setInputValue] = useState("");
+	const [ todos, setTodos ] = useState([]);
 	return (
 		<div className="container">
-			<h1>My Todo</h1>
+			<h1>Todo {inputValue} </h1>
 			<ul>
-				<li><input type = "text" placeholder="Whats need to be done?"></input></li>
-				<li>Wash my hands</li>
-				<li>Eat</li>
-				<li>Walk the dog</li>
+				<li><input type = "text" 
+					placeholder="Whats need to be done?"
+					onKeyPress={(e)=>{
+						if (e.key === "Enter") {
+							setTodos(todos.concat(inputValue));
+						}
+					}}
+					onChange={(e) => setInputValue(e.target.value)} value={inputValue}>
+						
+					</input>
+				</li>
+				{todos.map((item, index) =>(
+				<li>
+					{item}{" "}
+					<i class="fa-solid fa-x"
+					 onclick = {() => 
+					 	setTodos(
+					 		todos.filter(
+					 			(t,currentIndex) => 
+					 				index != currentIndex))}
+					></i>
+				</li>
+				))}
 			</ul>
 			<div>23 task</div>
         </div>
