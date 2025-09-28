2c67f72c92af5098b36e5d39f45f0443b2448ed3
Esteban Ossandón <estebanossandon@192.168.1.45>
2025-09-28 16:19:06 -0300

    finalizado

--- src/js/components/List.jsx
+++ src/js/components/List.jsx
@@ -6,6 +6,43 @@ const List = () => {
   const [todos, setTodos] = useState([]);
   const [esconder, setEsconder] = useState(null);
   const API_URL = "https://playground.4geeks.com/todo";
+  const user = "esteban"
+
+
+  const getAllUsers = async () => {
+    try {
+      const response = await fetch(`${API_URL}/users/`, {
+        method: "GET",
+      });
+      const data = await response.json();
+      if (!data.users.find(index => index.name === 'esteban')) {
+        createUser();
+      } else {
+        getAllTasks();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createUser = async () => {
+
+    try {
+      const response = await fetch(`${API_URL}/users/esteban`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          label: user,
+          done: false,
+        }),
+      })
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
 
   const getAllTasks = async () => {
     try {
@@ -14,9 +51,7 @@ const List = () => {
       });
       const data = await response.json();
       setTodos(data.todos.reverse());
-      console.log(data.todos);
       setInputValue("");
-      getAllTasks;
     } catch (error) {
       console.log(error);
     }
@@ -24,7 +59,7 @@ const List = () => {
 
   const postTask = async (event) => {
     try {
-      if (event.key === "Enter") {
+      if (event.key === "Enter" && inputValue != "") {
         const response = await fetch(`${API_URL}/todos/esteban`, {
           method: "POST",
           headers: {
@@ -36,15 +71,45 @@ const List = () => {
           }),
         });
         getAllTasks();
-        console.log(response);
       }
     } catch (error) {
       console.log(error);
     }
   };
 
+  const puthTask = async (id) => {
+    try {
+      if (inputValue != "") {
+        const response = await fetch(`${API_URL}/todos/${id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            label: inputValue,
+            is_done: true,
+          }),
+        });
+        getAllTasks();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const delTask = async (id) => {
+    try {
+      const response = await fetch(`${API_URL}/todos/${id}`, {
+        method: "DELETE",
+      });
+      getAllTasks();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    getAllTasks();
+    getAllUsers();
   }, []);
 
   return (
@@ -66,15 +131,16 @@ const List = () => {
             onMouseOver={() => setEsconder(index)}
             onMouseLeave={() => setEsconder(null)}
           >
+            {esconder === index && (
+              <input className="check"
+                type="checkBox"
+                onChange={e => puthTask(item.id)} />
+            )}
             {item.label}{" "}
             {esconder === index && (
               <i
                 className="fa-solid fa-x ex"
-                onClick={() =>
-                  setTodos(
-                    todos.filter((t, currentIndex) => index != currentIndex)
-                  )
-                }
+                onClick={() => delTask(item.id)}
               ></i>
             )}
           </li>
@@ -82,6 +148,7 @@ const List = () => {
       </ul>
       <div> {todos.length} Item Left </div>
     </div>
+
   );
 };
 
